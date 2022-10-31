d8cae8ae7670766ef289cb44aa81eb9c0961f2f9
inayatullahaaxioned <inayatullaha@axioned.com>
2022-10-31 12:07:46 +0530

    js logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 /* Author: 
-
+Inayatullah
 */
+//variables
+var Box = document.querySelector('.sound-box'),
+  btn, active;
 
+// adding event listener 
+Box.addEventListener('click', playSong);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function for playing and stopping the sounds 
+function playSong(e) {
+  active = document.querySelector('.active');
+  if (active) {
+    active.pause();
+    active.classList.remove('active');
+  }
+  if (e.target != this) {
+    btn = e.target.children[0];
+    btn.classList.add('active');
+    btn.play();
+  }
+};
